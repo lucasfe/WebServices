9fe176e99ea383a17074b2e32701953a15c68d16
Lucas Ferreira <lucasfe@gmail.com>
2014-06-27 19:13:41 -0300

    Initial Commit

---
+++ WebServicesTests/WebServicesTests.m
@@ -0,0 +1,34 @@
+//
+//  WebServicesTests.m
+//  WebServicesTests
+//
+//  Created by ios4357 on 27/06/14.
+//  Copyright (c) 2014 lgrmobile. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebServicesTests : XCTestCase
+
+@end
+
+@implementation WebServicesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
